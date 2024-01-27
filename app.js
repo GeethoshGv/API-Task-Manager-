6a660b1571ca2c5913a584c08394c0ef7c0fc0fb
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-27 15:15:13 +0530

    basic server setup

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
 const express = require("express");
-
 const app = express();
 
-const tasks = require("./routes/task");
+app.get("/", (req, res) => {
+  res.send("home page");
+});
 
-const connectDB = require("./DB/connect");
-
-require("dotenv").config();
-app.use(express.json());
-
-app.use("/api/v1/tasks", tasks);
-
-const port = 5000;
-
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`server is listening ${port}`));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+port = 5000;
+app.listen(port, console.log(`server is listening in ${port}`));
