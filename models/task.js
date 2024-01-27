426faeea4805f359cc22fcdfe2336bd4ebcffc27
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-28 00:11:18 +0530

    created a schema for Testing

--- models/task.js
+++ models/task.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({});
+const TaskSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
