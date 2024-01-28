b623039215c2528d2009f5fd03e2195f747c966d
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-28 16:22:05 +0530

    Added try-catch blocks for error handling in getalltask and createtask routes and used mongoose static helper function Model.find() for getting all task from database

--- controller-logic/task.js
+++ controller-logic/task.js
@@ -1,7 +1,12 @@
 const Task = require("../models/task");
 
-const getalltask = (req, res) => {
-  res.send("get all task");
+const getalltask = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const createtask = async (req, res) => {
   try {
