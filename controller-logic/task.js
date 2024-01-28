c076c8f82b85ca02eac92ff5aa36ac011dedd483
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-28 17:24:03 +0530

    error handling is implemented to respond with a 404 status when the requested task is not found. This adds valuable functionality to the task-related routes.

--- controller-logic/task.js
+++ controller-logic/task.js
@@ -8,6 +8,7 @@ const getalltask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
+
 const createtask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
@@ -16,8 +17,19 @@ const createtask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-const gettask = (req, res) => {
-  res.send("get single task");
+const gettask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
+    }
+
+    res.status(200).send({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const updateTask = (req, res) => {
   res.send("update task");
